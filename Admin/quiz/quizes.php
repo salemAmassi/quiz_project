7995f067be86611ae0c01251d775ad3c3f07cf6b
aboodsido <odod41907@gmail.com>
2022-05-19 14:11:30 +0300

    done with admin.quizes

--- Admin/quiz/quizes.php
+++ Admin/quiz/quizes.php
@@ -1,8 +1,149 @@
 <?php
-require_once('config.php');
+require_once('../../config.php');
 //Salem: get all quizes and display them 
-$getQuizesQuery  = "SELECT * FROM quiz";
 
 
+$adminId = $_GET['id'];
+$quizId = $_GET['quizId'];
 
 ?>
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
+      <!--  All snippets are MIT license http://bootdey.com/license -->
+      <title>Quizes Page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body>
+  
+  <div class="container">
+      <div class="main-body">
+      
+            <!-- Breadcrumb -->
+            <nav aria-label="breadcrumb" class="main-breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Quizes</li>
+              </ol>
+            </nav>
+            <!-- /Breadcrumb -->
+      
+            <div class="row gutters-sm">
+                <?php $sql = "SELECT q.title as Quiz_Title  , q.created_time as Craeted_Time , s.name as Subject_Name 
+                FROM (quiz as q JOIN subject as s ON q.subjectId = s.id ) JOIN admin as a ON a.id = q.adminId WHERE a.id= $adminId";
+                    $sqlResult = $connection->query($sql);
+
+                    if($sqlResult->num_rows > 0){
+                      while($row = $sqlResult->fetch_assoc()){ 
+                        $title = $row['Quiz_Title'];
+                        $created_time = $row['Craeted_Time'];
+                        $subjectName = $row['Subject_Name'];
+                    echo '
+                  <div class="col-sm-6 mb-3">
+                    <div class="card h-100">
+                      <div class="card-body">
+                        <h5 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Registered Quiz </i>';?><?php echo $title; ?> <?php echo '</h5>
+                        <h6>Subject</h6>
+                        <div class="col-sm-9 text-secondary">'; ?>
+                      <?php echo $subjectName; ?>
+                      <?php echo '
+                      </div><hr>
+                      <h6>Created Time</h6>
+                        <div class="col-sm-9 text-secondary">
+                        '; ?>
+                        <?php echo $created_time; ?>
+                      <?php   echo '
+                      </div><hr>
+                      <form method="POST" action="">
+                      <input type="submit" value="Delete" name="subDelete"/>
+                      </form>
+                     </div>
+                     
+                    </div>
+                    
+                  </div>
+                ';
+                  }
+                }
+                ?>
+
+              </div>   
+              </div>
+              
+          </div>
+      </div>
+  
+  <style type="text/css">
+  body{
+      margin-top:20px;
+      color: #1a202c;
+      text-align: left;
+      background-color: #e2e8f0;    
+  }
+  .main-body {
+      padding: 15px;
+  }
+  .card {
+      box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);
+  }
+  
+  .card {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      min-width: 0;
+      word-wrap: break-word;
+      background-color: #fff;
+      background-clip: border-box;
+      border: 0 solid rgba(0,0,0,.125);
+      border-radius: .25rem;
+  }
+  
+  .card-body {
+      flex: 1 1 auto;
+      min-height: 1px;
+      padding: 1rem;
+  }
+  
+  .gutters-sm {
+      margin-right: -8px;
+      margin-left: -8px;
+  }
+  
+  .gutters-sm>.col, .gutters-sm>[class*=col-] {
+      padding-right: 8px;
+      padding-left: 8px;
+  }
+  .mb-3, .my-3 {
+      margin-bottom: 1rem!important;
+  }
+  
+  .bg-gray-300 {
+      background-color: #e2e8f0;
+  }
+  .h-100 {
+      height: 100%!important;
+  }
+  .shadow-none {
+      box-shadow: none!important;
+  }
+  input[type=submit] {
+    padding:5px 15px; 
+    background:#ccc; 
+    border:0 none;
+    cursor:pointer;
+    -webkit-border-radius: 5px;
+    border-radius: 5px; 
+}
+  </style>
+  
+  <script type="text/javascript">
+  
+  </script>
+  </body>
+  </html>
