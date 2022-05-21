0983aaca3d90c5869c891b68e49091cc285ec05d
salem <salem.amassi@gmail.com>
2022-05-21 10:43:30 +0300

    create: quizCreate.php, addQuizQuestions.php, insertQuestions.php, problem: cant go from addQuizQuestions.php to insertQuestions.php

--- Admin/quiz/quizCreate.php
+++ Admin/quiz/quizCreate.php
@@ -1,173 +1,96 @@
-<?php
-require_once("../../config.php"); 
-if(isset($_SESSION['username'])){
- echo   '<form action='.$_SERVER["PHP_SELF"].' method = "post" >
-Enter number of questions: <input type="text" name= "noOfQuestions">
-<input type="submit" value="Go" name="submit">
-
- </form>';
-
-}
-
-
-
-
-?>
-
-<form action="$_SERVER['PHP_SELF']" method="post">
-
-<?php
-
-
-?>
-
-</form>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-*,
-*:after,
-*:before {
-	box-sizing: border-box;
-}
-
-$primary-color: #00005c; // Change color here. C'mon, try it! 
-$text-color: mix(#000, $primary-color, 64%);
-
-body {
-	font-family: "Inter", sans-serif;
-	color: $text-color;
-	font-size: calc(1em + 1.25vw);
-	background-color: mix(#fff, $primary-color, 90%);
-    display: flex; 
-    flex-direction: column;
-}
-
-form {
-	display: flex;
-	flex-wrap: wrap;
-	flex-direction: column;
-}
-
-label {
-	display: flex;
-	cursor: pointer;
-	font-weight: 500;
-	position: relative;
-	overflow: hidden;
-	margin-bottom: 0.375em;
-	/* Accessible outline */
-	/* Remove comment to use */ 
-	/*
-		&:focus-within {
-				outline: .125em solid $primary-color;
-		}
-	*/
-	input {
-		position: absolute;
-		left: -9999px;
-		&:checked + span {
-			background-color: mix(#fff, $primary-color, 84%);
-			&:before {
-				box-shadow: inset 0 0 0 0.4375em $primary-color;
-			}
-		}
-	}
-	span {
-		display: flex;
-		align-items: center;
-		padding: 0.375em 0.75em 0.375em 0.375em;
-		border-radius: 99em; // or something higher...
-		transition: 0.25s ease;
-		&:hover {
-			background-color: mix(#fff, $primary-color, 84%);
-		}
-		&:before {
-			display: flex;
-			flex-shrink: 0;
-			content: "";
-			background-color: #fff;
-			width: 1.5em;
-			height: 1.5em;
-			border-radius: 50%;
-			margin-right: 0.375em;
-			transition: 0.25s ease;
-			box-shadow: inset 0 0 0 0.125em $primary-color;
-		}
-	}
-}
-
-// Codepen spesific styling - only to center the elements in the pen preview and viewport
-.container {
-	/* position: absolute;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0; */
-	width: 50%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding: 20px;
-    margin: auto;
-}
-// End Codepen spesific styling
-</style>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
 </head>
 <body>
-<?php
-if(isset($_POST['submit'])){
-    echo '<form action='.$_SERVER["PHP_SELF"].' method = "post">';
-    $noOfQuestions = $_POST['noOfQuestions'];
-for ($i=0; $i <$noOfQuestions ; $i++) { 
-        echo '<div class="container">
-        Question '.($i+1).'
-        <label>
-        <input type="text" name="question'.$i.'" placeholder="qustion content"/>
-    </label>
-            <label>
-                <input type="radio" name="answer'.$i.'"  />
-                <span>
-                <input type="text" name="fAnswer" placeholder="choice 1">
-                </span>
-            </label>
-            <label>
-                <input type="radio" name="answer'.$i.'" />
-                <span><input type="text" name="sAnswer" placeholder="choice 2">
-                </span>
-            </label>
-            <label>
-                <input type="radio" name="answer'.$i.'"/>
-                <span><input type="text" name="thAnswer" placeholder="choice 3">
-                </span>
-            </label>
-            <label>
-                <input type="radio" name="answer'.$i.'"/>
-                <span><input type="text" name="fthAnswer" placeholder="choice 4">
-                </span>
-            </label>
-    </div>
-    <hr>';
-}
-echo 
-'<input type="submit" name="submit_create" value="create"></form>';
-}?>
+<div class="row">
+<span class="title1" style="margin-left:40%;font-size:30px;"><b>Enter Quiz Details</b></span><br /><br />
+ <div class="col-md-3"></div><div class="col-md-6">   
+ <form class="form-horizontal title1" name="form" action="addQuizQuestions.php" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="name"></label>  
+  <div class="col-md-12">
+  <input id="name" name="name" placeholder="Enter Quiz title" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="total"></label>  
+  <div class="col-md-12">
+  <input id="total" name="total" placeholder="Enter total number of questions" class="form-control input-md" type="number" min="1">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="right"></label>  
+  <div class="col-md-12">
+  <input id="right" name="rightMark" placeholder="Enter marks on right answer" class="form-control input-md" min="0" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="wrong"></label>  
+  <div class="col-md-12">
+  <input id="wrong" name="wrongMark" placeholder="Enter minus marks on wrong answer without sign" class="form-control input-md" min="0" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="time"></label>  
+  <div class="col-md-12">
+  <input id="time" name="time" placeholder="Enter time limit for test in minute" class="form-control input-md" min="1" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="tag"></label>  
+  <div class="col-md-12">
+  <input id="tag" name="tag" placeholder="Enter #tag which is used for searching" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="desc"></label>  
+  <div class="col-md-12">
+  <textarea rows="8" cols="8" name="desc" class="form-control" placeholder="Write description here..."></textarea>  
+  </div>
+</div>
+
+
+<div class="form-group">
+  <label class="col-md-12 control-label" for=""></label>
+  <div class="col-md-12"> 
+    <input  type="submit" style="margin-left:45%"  name = "submit" class="btn btn-primary" value="Submit" class="btn btn-primary"/>
+  </div>
+</div>
+
+</fieldset>
+</form></div>
+
 
 </body>
 </html>
-<?php
-if(isset($_POST['submit_create'])){
-echo "<pre>"; 
-print_r($_POST);
-echo"</pre>";    
-
-}
-
-?>
-
